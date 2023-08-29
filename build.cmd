b2c244bef7b2881a9d9a0a3f883cadf193db17ea
begomes-wortenpt <begomes@worten.pt>
2023-08-29 17:11:23 +0100

    Added Tray Icon Options and Config File

--- build.cmd
+++ build.cmd
@@ -1,4 +1,3 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -lib:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" -r:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Microsoft.VisualBasic.dll" -target:winexe -out:"topleft.exe" /win32icon:"question_answer_white_24dp.ico"  "topnotify.cs"
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -lib:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" -r:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Microsoft.VisualBasic.dll" -target:winexe -out:"topright.exe" /win32icon:"question_answer_white_24dp.ico" "topnotify.cs"
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -lib:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" -r:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Microsoft.VisualBasic.dll" -target:winexe -out:"topnotify.exe" /win32icon:"question_answer_white_24dp.ico"  "topnotify.cs"
 
 PAUSE
--- topnotify.cs
+++ topnotify.cs
@@ -4,21 +4,22 @@ using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 public class Program {
 
-    #region Native Stuff
     [DllImport("user32.dll", SetLastError = true)]
     static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
     [DllImport("user32.dll", SetLastError = true)]
-    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr hWndChildAfter, string className,  string windowTitle);
+    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr hWndChildAfter, string className, string windowTitle);
 
     [DllImport("user32.dll")]
     static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
     [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-    public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+    public static extern IntPtr SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
 
     [DllImport("user32.dll")]
     public static extern bool GetWindowRect(IntPtr hwnd, ref Rectangle rectangle);
@@ -28,89 +29,70 @@ public class Program {
     const short SWP_NOZORDER = 0X4;
     const int SWP_SHOWWINDOW = 0x0040;
 
-    #endregion
+    public static int Cooldown;
 
-    public static int Cooldown; // Since The Teams Notification Stutters, Add A Cooldown To Dismiss
+    private static string SettingsFilePath {
+        get {
+            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "settings.ini");
+        }
+    }
+
+    private static NotificationPosition LoadNotificationPosition() {
+        if (File.Exists(SettingsFilePath)) {
+            string[] lines = File.ReadAllLines(SettingsFilePath);
+            foreach (string line in lines) {
+                if (line.StartsWith("trayposition")) {
+                    string[] parts = line.Split('=');
+                    if (parts.Length == 2) {
+                        string positionStr = parts[1].Trim().ToLower();
+                        if (positionStr == "topleft") {
+                            return NotificationPosition.TopLeft;
+                        } else if (positionStr == "topright") {
+                            return NotificationPosition.TopRight;
+                        }
+                    }
+                }
+            }
+        }
+
+        return NotificationPosition.TopRight;
+    }  
+
+    private static void SaveNotificationPosition(NotificationPosition position) {
+        string positionStr = position == NotificationPosition.TopLeft ? "topleft" : "topright";
+        string[] lines = { "trayposition = " + positionStr };
+
+        File.WriteAllLines(SettingsFilePath, lines);
+    }
 
     public static void Main(string[] args) {
         NotifyIcon notifyIcon = new NotifyIcon();
-
-        // Load the custom icon directly using Icon class
-        // Using custom icon from Google Material Icons
-        // Icon source: https://fonts.google.com/icons?selected=Material%20Icons%3Aquestion_answer%3A
         notifyIcon.Icon = new Icon("question_answer_white_24dp.ico");
         notifyIcon.Visible = true;
 
-        // Create the context menu for the taskbar icon
-        ContextMenu contextMenu = new ContextMenu();
+        NotificationPosition initialPosition = LoadNotificationPosition();
+
+        MenuItem topLeftMenuItem = new MenuItem("Top Left");
+        MenuItem topRightMenuItem = new MenuItem("Top Right");
         MenuItem exitMenuItem = new MenuItem("Exit");
-        exitMenuItem.Click += new EventHandler(ExitMenuItem_Click);
+
+        ContextMenu contextMenu = new ContextMenu();
+        contextMenu.MenuItems.Add(topLeftMenuItem);
+        contextMenu.MenuItems.Add(topRightMenuItem);
         contextMenu.MenuItems.Add(exitMenuItem);
+
+        SetCheckmarks(initialPosition, topLeftMenuItem, topRightMenuItem);
+
+        topLeftMenuItem.Click += (sender, e) => ChangePosition(NotificationPosition.TopLeft, topLeftMenuItem, topRightMenuItem);
+        topRightMenuItem.Click += (sender, e) => ChangePosition(NotificationPosition.TopRight, topLeftMenuItem, topRightMenuItem);
+        exitMenuItem.Click += new EventHandler(ExitMenuItem_Click);
+
         notifyIcon.ContextMenu = contextMenu;
 
-        // Run the main logic in another task to avoid blocking the main one with the while loop.
         Task.Run(() => {
             while (true) {
-
-                //MS Teams Notifications
-                try{
-                    var teamsHwnd = FindWindow("Chrome_WidgetWin_1", "Microsoft Teams Notification");
-                    var chromeHwnd = FindWindowEx(teamsHwnd, IntPtr.Zero, "Chrome_RenderWidgetHostHWND", "Chrome Legacy Window"); // I Think MS Is Using Chrome Webview For Notifications
-
-                    //If A Notification Is Showing Up
-                    if (chromeHwnd != IntPtr.Zero){
-                        Cooldown = 0;
-
-                        if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
-                            //Sets to top left
-                            SetWindowPos(teamsHwnd, 0, 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                        }
-                        else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
-                            //Sets to top right
-
-                            //Get the current position of the notification window
-                            Rectangle NotifyRect = new Rectangle();
-                            GetWindowRect(teamsHwnd, ref NotifyRect);
-
-                            NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
-                            NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
-
-                            SetWindowPos(teamsHwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width - 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                        }
-                    }
-                    else {
-                        if (Cooldown >= 30){
-                            SetWindowPos(teamsHwnd, 0, 0, -9999, -9999, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW); // Move To Off Screen
-                            Cooldown = 0;
-                        }
-                        Cooldown += 1; // Don't Dismiss Until 30 Frames After Signal, Prevents Stutters
-                    }
-                }
-                catch{
-                    //User Doesn't Have Teams
-                }
-
-                //Windows System Notifications
-                var hwnd = FindWindow("Windows.UI.Core.CoreWindow", "New notification");
-                if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
-                    //Sets to top left (easy peasy)
-                    //50PX Y offset to make the spacing even
-                    SetWindowPos(hwnd, 0, 0, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                }
-                else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
-                    //Sets to top right (not as easy)
-
-                    //Get the current position of the notification window
-                    Rectangle NotifyRect = new Rectangle();
-                    GetWindowRect(hwnd, ref NotifyRect);
-
-                    NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
-                                NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
-
-                    //50PX Y offset to make the spacing even
-                    SetWindowPos(hwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                }
-
+                HandleTeamsNotifications(LoadNotificationPosition());
+                HandleSystemNotifications(LoadNotificationPosition());
                 Thread.Sleep(10);
             }
         });
@@ -118,7 +100,79 @@ public class Program {
         Application.Run();
     }
 
+    private static void SetCheckmarks(NotificationPosition position, MenuItem topLeftMenuItem, MenuItem topRightMenuItem) {
+        topLeftMenuItem.Checked = position == NotificationPosition.TopLeft;
+        topRightMenuItem.Checked = position == NotificationPosition.TopRight;
+    }
+
+    private static void ChangePosition(NotificationPosition newPosition, MenuItem topLeftMenuItem, MenuItem topRightMenuItem) {
+        SetNotificationPosition(newPosition);
+        SetCheckmarks(newPosition, topLeftMenuItem, topRightMenuItem);
+        SetTrayIconPosition(newPosition);
+    }
+
+    private static void SetNotificationPosition(NotificationPosition position) {
+        SaveNotificationPosition(position);
+    }
+
+    private static void SetTrayIconPosition(NotificationPosition position) {
+        int x = position == NotificationPosition.TopLeft ? 15 : Screen.PrimaryScreen.Bounds.Width - 115;
+        var hwnd = FindWindow("Shell_TrayWnd", null);
+        if (hwnd != IntPtr.Zero) {
+            SetWindowPos(hwnd, IntPtr.Zero, x, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+        }
+    }
+
+    private static void HandleTeamsNotifications(NotificationPosition position) {
+        try {
+            var teamsHwnd = FindWindow("Chrome_WidgetWin_1", "Microsoft Teams Notification");
+            var chromeHwnd = FindWindowEx(teamsHwnd, IntPtr.Zero, "Chrome_RenderWidgetHostHWND", "Chrome Legacy Window");
+
+            if (chromeHwnd != IntPtr.Zero) {
+                Cooldown = 0;
+
+                int x = position == NotificationPosition.TopLeft ? 15 : Screen.PrimaryScreen.Bounds.Width - 115;
+
+                Rectangle notificationRect = GetWindowRectangle(teamsHwnd);
+                int notificationWidth = notificationRect.Width - notificationRect.X;
+
+                if (position == NotificationPosition.TopRight) {
+                    x = Screen.PrimaryScreen.Bounds.Width - notificationWidth - 15;
+                }
+
+                SetWindowPos(teamsHwnd, IntPtr.Zero, x, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+            } else {
+                if (Cooldown >= 30) {
+                    SetWindowPos(teamsHwnd, IntPtr.Zero, 0, -9999, -9999, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                    Cooldown = 0;
+                }
+                Cooldown += 1;
+            }
+        } catch {
+            // User Doesn't Have Teams
+        }
+    }
+
+    private static Rectangle GetWindowRectangle(IntPtr hwnd) {
+        Rectangle rectangle = new Rectangle();
+        GetWindowRect(hwnd, ref rectangle);
+        return rectangle;
+    }
+
+    private static void HandleSystemNotifications(NotificationPosition position) {
+        var hwnd = FindWindow("Windows.UI.Core.CoreWindow", "New notification");
+        int x = position == NotificationPosition.TopLeft ? 0 : Screen.PrimaryScreen.Bounds.Width - 150;
+
+        SetWindowPos(hwnd, IntPtr.Zero, x, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+    }
+
     private static void ExitMenuItem_Click(object sender, EventArgs e) {
         Application.Exit();
     }
+
+}
+
+public enum NotificationPosition {
+    TopRight,
+    TopLeft
 }
